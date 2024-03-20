8a38f8dad536211b41dfc1f86ec9b7c199bc5e8f
LiXiang <rickanmorty@163.com>
2024-03-21 02:05:45 +0800

    commit in 3/21 night

--- Basic_Algorithm/array_element_target_addtion.cpp
+++ Basic_Algorithm/array_element_target_addtion.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-signed main() {
+signed main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
@@ -12,17 +13,22 @@ signed main() {
 
     vector<int> A(n), B(m);
 
-    for (int i = 0; i < n; i++) cin >> A[i];
-    for (int j = 0; j < m; j++) cin >> B[j];
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+    for (int j = 0; j < m; j++)
+        cin >> B[j];
 
-    //指针a向右移动，若要满足A[a]+B[b]>x且b取最小值,b只能向左移动，满足单调性
-    for (int a = 0, b = m - 1; a < n; a++) {
-        while (b >= 0 && A[a] + B[b] > x) b--;
-        if (A[a] + B[b] == x) {
+    // 指针a向右移动，若要满足A[a]+B[b]>x且b取最小值,b只能向左移动，满足单调性
+    for (int a = 0, b = m - 1; a < n; a++)
+    {
+        while (b >= 0 && A[a] + B[b] > x)
+            b--;
+        if (A[a] + B[b] == x)
+        {
             cout << a << " " << b;
             break;
         }
     }
-    //时间复杂度O(n+m)
+    // 时间复杂度O(n+m)
     return 0;
 }
