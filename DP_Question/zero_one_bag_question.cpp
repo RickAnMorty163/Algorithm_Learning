3484ea2ff76d17355ba9f7eede847aac10295551
LiXiang <rickanmorty@163.com>
2024-03-22 09:49:09 +0800

    DP优化

--- DP_Question/zero_one_bag_question.cpp
+++ DP_Question/zero_one_bag_question.cpp
@@ -8,6 +8,7 @@ int n, m;
 
 int v[N], w[N]; // v->体积 w->价值
 int dp[N][N];
+int dp_space[N];
 
 signed main()
 {
@@ -18,18 +19,26 @@ signed main()
     for (int i = 1; i <= n; i++)
         std::cin >> v[i] >> w[i];
 
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j < -m; j++)
-        {
-            if (w[i] > j)
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
-            else
-            {
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v[i]] + w[j]);
-            }
-        }
+    // for (int i = 1; i <= n; i++)
+    //     for (int j = 1; j < -m; j++)
+    //     {
+    //         if (w[i] > j)
+    //         {
+    //             dp[i][j] = dp[i - 1][j];
+    //         }
+    //         else
+    //         {
+    //             dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v[i]] + w[j]);
+    //         }
+    //     }
+    // std::cout << dp[n][m];
 
-    std::cout << dp[n][m];
+    // 优化DP空间复杂度
+    for (int i = 1; i <= n; ++i)        // 逆序j是因为j的状态转移依赖于前一个状态，而在顺序遍历中j的前一个状态很有可能已经被污染
+        for (int j = 1; j >= v[i]; --j) // j<v[i]不进入下一个状态，相当于保留状态，即处理了j<v[i]的状态转移问题
+            dp_space[j] = max(dp_space[j], dp[j - v[i]]);
+
+    cout << dp_space[m];
+
+    return 0;
 }
